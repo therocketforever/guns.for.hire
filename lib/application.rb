b52ef9b88057f1ba226fe2ef250f43dcb408d32b
DON who is a ghost <therocketforever@me.com>
2013-09-21 17:26:23 -0400

    Why the fuck dosnt this damn thing work?

--- lib/application.rb
+++ lib/application.rb
@@ -1,13 +1,27 @@
 class Application < Sinatra::Base
 
-  enable :inline_templates,:logging
+  enable :logging
 
   configure(:production) { Bundler.require(:production) }
   configure(:development) { Bundler.require(:development) }
   configure(:test) {}
 
   get '/' do
-    slim :index
+    slim :blue
   end
+
+  get '/dot' do
+    "dot"
+  end
+
+  get '/space' do
+    "space"
+  end
+
+  get '/blue' do
+    "blue"
+  end
+
+
 
 end
